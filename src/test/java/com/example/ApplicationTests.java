68da8119d75005a8bbeee76008dd1f2df15212c4
Wayne <zwe3854335@gmail.com>
2025-11-12 16:51:32 +0800

    Remove Spring Shell dependency and related HelloWorldCommand; cleanup unused properties and adjust tests accordingly.

--- src/test/java/com/example/ApplicationTests.java
+++ src/test/java/com/example/ApplicationTests.java
@@ -1,16 +1,18 @@
 package com.example;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(classes = {Application.class})
 @ActiveProfiles("test")
 class ApplicationTests {
 
-    @Test
-    void contextLoads() {
-        // This test will fail if the application context cannot start
-    }
-
+  @Test
+  void contextLoads() {
+    // This test will fail if the application context cannot start
+  }
 }
